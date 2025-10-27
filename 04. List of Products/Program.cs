fa31ed9737acec7673b60dd22fd1c575690fa5cb
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-27 16:58:24 +0200

    Added a New Project

---
+++ 04. List of Products/Program.cs
@@ -0,0 +1,17 @@
+﻿int input = int.Parse(Console.ReadLine());
+string product = string.Empty;
+int count = 1;
+List<string> shoppingList = new List<string>();
+
+for (int i = 1; i <= input; i++)
+{ 
+    product = Console.ReadLine();
+    shoppingList.Add(product);
+
+}
+shoppingList.Sort();
+foreach (string products in shoppingList)
+{
+    Console.WriteLine($"{count}.{products}");
+    count++;
+}
