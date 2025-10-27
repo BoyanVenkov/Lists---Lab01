c61c9dc7812c8028d02e3b5e51967c285bc714da
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-27 07:03:26 +0200

    Add project files.

---
+++ 01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿List<double> input = Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .ToList();
+
+for (int i = 0; i < input.Count - 1; i++)
+{
+    double sum = 0;
+    if (input[i] == input[i + 1])
+    {
+        sum = input[i] + input[i + 1];
+        input.RemoveAt(i + 1);
+        input.RemoveAt(i);
+        input.Insert(i, sum);
+
+        i = -1;
+        sum = 0;
+    }
+}
+Console.WriteLine(String.Join(" ", input));
