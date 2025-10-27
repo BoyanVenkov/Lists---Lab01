2cdbf9de2c6643810348c74ba739d12073fc0398
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-27 17:19:37 +0200

    Added a New Project

---
+++ 05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> output = new List<int>();
+int negativeCount = 0;
+
+for (int i = 0; i < input.Count; i++)
+{
+    if (input[i] >= 0)
+    {
+        output.Add(input[i]);
+        negativeCount++;
+    }
+}
+if (negativeCount == 0)
+{
+    Console.WriteLine("empty");
+    return;
+}
+output.Reverse();
+Console.WriteLine(string.Join(" ", output));
